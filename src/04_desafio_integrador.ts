920ed52c6c5dfa85ca9a0a51c916b53cb22350ce
MerchanDaniel <daniel.merchanm.est@uets.edu.ec>
2026-09-15 05:27:50 +0000

    feat(reto-04): completar logica de carrito del bar salesiano

--- src/04_desafio_integrador.ts
+++ src/04_desafio_integrador.ts
@@ -70,13 +70,24 @@ export interface ResumenFinanciero {
  * Todos los valores numéricos deben retornar redondeados a 2 decimales: Number(val.toFixed(2)).
  */
 export function calcularTotalesPedido(pedido: PedidoMovil): ResumenFinanciero {
-  // 👇 TODO: Escribe tu lógica de cálculo aquí y reemplaza el objeto por defecto:
+ const subtotal = pedido.detalles.reduce(
+    (acc, item) => acc + item.producto.precioUnitario * item.cantidad,
+    0
+  );
+
+  const descuentoEstudiantil = subtotal >= 10.0 ? subtotal * 0.1 : 0;
+  const baseImponible = subtotal - descuentoEstudiantil;
+  const iva15 = baseImponible * 0.15;
+  const totalPagar = baseImponible + iva15;
+
   return {
-    subtotal: 0,
-    descuentoEstudiantil: 0,
-    iva15: 0,
-    totalPagar: 0
+    subtotal: Number(subtotal.toFixed(2)),
+    descuentoEstudiantil: Number(descuentoEstudiantil.toFixed(2)),
+    iva15: Number(iva15.toFixed(2)),
+    totalPagar: Number(totalPagar.toFixed(2))
   };
+
+ 
 }
 
 /**
